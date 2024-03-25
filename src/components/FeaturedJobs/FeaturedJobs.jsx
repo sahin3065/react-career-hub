acf3ab96c003dd346ab081c78aac62b661e3c454
sahin3065 <sahinalom5161@gmail.com>
2024-03-25 15:23:38 +0600

    add icons and handle show all jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,8 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
   const [jobs,setJobs] = useState([]);
+  // this is not the best way to load show all data
+  const [dataLength,setDataLength] = useState(4);
 
   useEffect(() =>{
     fetch('jobs.json')
@@ -19,8 +21,11 @@ const FeaturedJobs = () => {
         </div>
         <div className="grid grid-cols-2 gap-6">
           {
-            jobs.map(job => <Job key={job.id} job={job}></Job>)
+            jobs.slice(0,dataLength).map(job => <Job key={job.id} job={job}></Job>)
           }
+        </div>
+        <div className={dataLength === jobs.length && 'hidden'}>
+            <button onClick={() => setDataLength(jobs.length)} className="btn bg-[#7E90FE] ">Show All Jobs</button>
         </div>
     </div>
   );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,10 +1,13 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollar } from "react-icons/ai";
+
 const Job = ({ job }) => {
   const { logo,job_title,company_name,remote_or_onsite,location,job_type,salary }=job;
 
   return (
-    <div className="card card-compact  bg-base-100 shadow-xl">
+    <div className="card  bg-base-100 shadow-xl">
       <figure>
-        <img src={logo} alt="Shoes" />
+        <img className="max-w-[116px]" src={logo} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{job_title}</h2>
@@ -12,6 +15,10 @@ const Job = ({ job }) => {
         <div>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+        </div>
+        <div className="mt-4 flex ">
+            <h2 className="flex mr-4"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+            <h2 className="flex"><AiOutlineDollar className="text-2xl"></AiOutlineDollar>{salary}</h2>
         </div>
         <div className="card-actions ">
           <button className="btn btn-primary bg-[#7E90FE]">View Details</button>
